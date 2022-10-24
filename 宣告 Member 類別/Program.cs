9b31ad3b4a8f698d54f9fbe959981fee1705cfa7
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 13:26:15 +0800

    加入專案檔案。

---
+++ 宣告 Member 類別/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 宣告_Member_類別
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	class Member
+	{
+		public string Name { get; set; }
+		public string Account { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+		public string Addres { get; set; }
+		public string Cellphone { get; set; }
+		public string Telephone { get; set; }
+
+		
+
+		public void Register(string Name, string Accout, string Passord, string ConfirmPassword, string email)
+		{
+
+		}
+
+		public void ForgetPassword(string Name, string Email)
+		{
+
+		}
+		public void Authenticate(string Name, string Email)
+		{
+
+
+		}
+	}
+}
